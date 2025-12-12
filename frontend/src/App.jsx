df9d2e73827b05477803c8f36821b0c42acd225a
Emmepuntato_ <matsantucci36@gmail.com>
2025-12-12 18:08:09 +0100

    changed root component from authForm to Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import AuthForm from './components/AuthForm.jsx'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProtectedRoutes from './utils/ProtectedRoutes.jsx'
 import Profile from './components/Profile.jsx'
+import Home from './components/Home.jsx'
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<AuthForm />} />
+        <Route path='/' element={<Home />} />
         <Route
           path='/profile'
           element={
